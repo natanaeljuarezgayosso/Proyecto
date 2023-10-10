782ecfb1a6a9b34d3be99096ae3eef115a8c3ade
miguelruizcruz <Angelruizc24@gmail.com>
2023-10-09 20:41:41 -0600

    parte de migue

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -14,7 +14,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("parte de miguel");
     }
     
 }
