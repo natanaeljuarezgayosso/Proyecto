960ec5ea613709b3b8be7f2d6533a3def076539a
miguelruizcruz <Angelruizc24@gmail.com>
2023-10-09 21:13:14 -0600

    miguel

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -17,6 +17,7 @@ public class Proyecto {
         System.out.println("Proyecto de Metodo Simplex");
         System.out.println("");
         System.out.println("Soy yo");
+        System.out.println("miguel");
     }
     
 }
